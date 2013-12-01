28f32101db7655d973d6eaf9ca49cb30a1723145
Shoghi Cervantes <shoghicp@gmail.com>
2013-12-01 20:26:20 +0100

    World Exporting part1

--- SimpleWorlds.php
+++ SimpleWorlds.php
@@ -3,9 +3,9 @@
 /*
 __PocketMine Plugin__
 name=SimpleWorlds
-description=Simple world loader & generator
+description=Simple world loader, generator, exporter
 version=0.1
-author=shoghicp
+author=PocketMine Team
 class=SimpleWorlds
 apiversion=7,8,9
 */
@@ -20,19 +20,27 @@ Small Changelog
 0.2
 - Unload support
 
+0.3
+- Export levels to MCPE format
+
 */
 
 
 
 class SimpleWorlds implements Plugin{
+	public static $chunkHeader = "base64://7c1Td6thEIbhado0TdIUSWbbtm3btm3btm3btm3btu37L+z1nn6z1nU4z60BIhoFKZEPldECfTEJy7EHF/EMv+ENEE2KXDwbnpZnpwl6YhwWYwfO4hG+I8wmmhDZUBoN0BWjbOb9+exswUncw2e4AkXjIhOKow46YhhmYwOO4lagef89O8FBojGRDoVRA20xCNOxBgdxDa9hs4tGRSq7eT8/O1XQEv0wGSuwF5fwHH/gCxZNhtyogKboFWzeH8/OEuzEOTzGD4Q7RBMhO8qgIbphNBZgK045zPv32fkCd4hoPGRGCdRFJwzHHGzEMdzGBzicorGc5v307BRBTbTDYMzAWhzCdbxBoEs0GlKjAKqilcu835+dKViJfbiMF/gLv1s0OfKgIpqhNyZgKXa5zfvn2XmCn4gIFU2MHCiLRuiOMViIbTiNB/iKUI95P75HNAtKoh46YwTmYhOO4w4+IiRMNDYyoChqhZn327MzBDOxDodxA28RFC4aHWlQENXQGgMwFavCzfv72bmCl5AIUUUK5EUlNEcfTMQy7MYFPMWvCPN+ZKRoEuREOTRGD4zFImzHGTzEN3i8ogmQFaW85v367HTBSMzDZpzAXXyC0ycaBxlRDLXRAUMxy2feX8/OEdzEO9j9ojGQFoVQHW0wENOwGgdwFa/8Yp111llnnXXWWfe/9w8=";
 	private $api, $config;
 	public function __construct(ServerAPI $api, $server = false){
 		$this->api = $api;
+		if(substr(self::$chunkHeader, 0, 9) === "base64://"){
+			self::$chunkHeader = gzinflate(base64_decode(substr(self::$chunkHeader, 9)));
+		}
 	}
 	
 	public function init(){
-		$this->api->console->register("simpleworlds", "load/unload <level> OR /simpleworlds generate <seed> <generator> <level>", array($this, "command"));
+		$this->api->console->register("simpleworlds", "<command> [parameters...]", array($this, "command"));
 		$this->api->console->alias("sw", "simpleworlds");
+		$this->api->console->alias("swe", "simpleworlds export");
 		$this->api->console->alias("swu", "simpleworlds unload");
 		$this->api->console->alias("swl", "simpleworlds load");
 		$this->api->console->alias("swg", "simpleworlds generate");
@@ -51,7 +59,11 @@ class SimpleWorlds implements Plugin{
 		$output = "";
 		if($cmd === "simpleworlds"){
 			if(count($params) < 2){
-				$output .= "Usage: /$cmd load <level> OR /simpleworlds generate <seed> <generator> <level>\n";
+				$output .= "Usage: /$cmd <command> [parameters...]\n";
+				$output .= "/$cmd load <levelName>: Loads a level on file.\n";
+				$output .= "/$cmd unload <levelName>: Safely unload a level.\n";
+				$output .= "/$cmd export <levelName>: Exports a loaded level to MCPE format.\n";
+				$output .= "/$cmd generate <seed> <generatorName> <levelName>: Generates a new world usign a generator with an specific seed.\n";
 				return $output;
 			}
 
@@ -74,6 +86,18 @@ class SimpleWorlds implements Plugin{
 						$output .= "Level loaded.\n";
 					}
 					break;
+				case "export":
+					$level = $this->api->level->get(implode(" ", $params));
+					if($level instanceof Level){
+						if($this->exportLevel($level) === false){
+							$output .= "Error exporting level.\n";
+						}else{
+							$output .= "Level correctly exported.\n";
+						}
+					}else{
+						$output .= "Level not loaded.\n";
+					}
+					break;
 				case "generate":
 					$seed = intval(array_shift($params));
 					$generator = $params[0] === "default" ? false:$params[0];
@@ -87,6 +111,45 @@ class SimpleWorlds implements Plugin{
 			}
 		}
 		return $output;
+	}
+	
+	public function exportLevel(Level $level){
+		@mkdir($this->api->plugin->configPath($this)."export/");
+		$path = $this->api->plugin->configPath($this)."export/".$level->getName()."/";
+		@mkdir($path);
+		$chunks = fopen($path."chunks.dat", "w");
+		fwrite($chunks, self::$chunkHeader);		
+		for($Z = 0; $Z < 16; ++$Z){
+			for($X = 0; $X < 16; ++$X){
+				$chunk = "";
+				$miniChunks = array();
+				for($Y = 0; $Y < 8; ++$Y){
+					$miniChunks[$Y] = $level->getMiniChunk($X, $Y, $Z);
+				}
+				$columns = array();
+				for($x = 0; $x < 16; ++$x){
+					for($z = 0; $z < 16; ++$z){
+						$index = ($x << 4) + $z;
+						$j = ($z << 4) + $x;
+						$columns[$index] = array("", "", str_repeat("\x00", 64), str_repeat("\x00", 64));
+						foreach($miniChunks as $raw){
+							$columns[$index][0] .= substr($raw, $j << 5, 16); //Block IDs
+							$columns[$index][1] .= substr($raw, ($j << 5) + 16, 8); //Block Metadata
+						}
+					}
+				}
+				for($i = 0; $i < 4; ++$i){
+					for($x = 0; $x < 16; ++$x){
+						for($z = 0; $z < 16; ++$z){
+							$chunk .= $columns[$x << 4 + $z][$i];
+						}
+					}
+				}
+				unset($columns, $miniChunks);
+				$chunk = str_pad($chunk, 86012, "\x00", STR_PAD_RIGHT);
+				fwrite($chunks, Utils::writeLInt(strlen($chunk)) . $chunk);
+			}
+		}
 	}
 
 	public function loadLevel($name){
